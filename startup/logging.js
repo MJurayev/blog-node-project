727855b89dd0a6f62ef15a6967ce9bd77e7c12cb
MJurayev <55982249+MJurayev@users.noreply.github.com>
2020-10-07 00:05:27 +0500

    Update logging.js

--- startup/logging.js
+++ startup/logging.js
@@ -5,7 +5,7 @@ require('winston-mongodb');
 module.exports = function () {
     winston.add(new winston.transports.Console());
     winston.add(new winston.transports.File({ filename: 'logs/vd-logs.log', level: 'error' }));
-    winston.add(new winston.transports.MongoDB({ db: 'mongodb+srv://deleted:21admin06@interpreter.0kfkg.mongodb.net/Mdata?retryWrites=true&w=majority', level: 'info' , 'useUnifiedTopology': true}));
+//     winston.add(new winston.transports.MongoDB({ db: 'mongodb+srv://deleted:21admin06@interpreter.0kfkg.mongodb.net/Mdata?retryWrites=true&w=majority', level: 'info' , 'useUnifiedTopology': true}));
     winston.exceptions.handle(new winston.transports.Console(), new winston.transports.File({ filename: 'logs/vd-logs.log' }))
     process.on('unhandledRejection', ex => {
         throw ex;
