b83c41c2da0e40330dc7272c849790b257e78dbd
MJurayev <55982249+MJurayev@users.noreply.github.com>
2020-10-07 00:12:59 +0500

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json());
 
-
+router.get('/', (req, res)=>{
+return res.send('server ishlayapti');
+});
 
 module.exports = router;
